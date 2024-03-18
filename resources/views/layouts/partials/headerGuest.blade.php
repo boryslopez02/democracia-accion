88e25daa34ec3b1e3c95cc199bec60ba503acf9f
Borys Lopez <borislopez1976@hotmail.com>
2024-03-18 19:11:03 -0400

    logo header

--- resources/views/layouts/partials/headerGuest.blade.php
+++ resources/views/layouts/partials/headerGuest.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
     <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'Laravel') }}
+        <a href="{{ url('/') }}" class="navbar-brand logo-img">
+            <img src="{{ asset('assets/images/logos/logo.png') }}" width="50" alt="" />
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
